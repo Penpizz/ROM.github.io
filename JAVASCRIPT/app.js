6e1cab646b2e227c94882c2a6c8a9c8d65245b5f
unknown <you@example.com>
2024-01-09 13:38:29 +0700

    changes

--- JAVASCRIPT/app.js
+++ JAVASCRIPT/app.js
@@ -43,7 +43,7 @@ window.signup = function (e) {
 
   createUserWithEmailAndPassword(auth, obj.email, obj.password)
   .then(function (success) {
-      window.location.replace('login.html')
+      window.location.replace('/HTML/login.html')
       // console.log(success.user.uid)
       alert("signup successfully")
     })
